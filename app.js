a6ac3bfcd95ca8227e902d1be62833d4ff92794b
Aucanon <aucanon@163.com>
2023-08-14 16:53:10 +0800

    引入中间件

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const express = require('express')
 const config =  require('./config/index')
+const cors = require('cors')
+const morgan = require('morgan')
+const express = require('express')
 
 const app = express()
+
+// 引入中间件
+app.use(express.json())
+
+app.use(cors())
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     res.send('ooo')
